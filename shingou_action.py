cf1a3586d608866703240fce1865f609ff698945
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-12-07 13:38:00 +0900

    コマンドライン演習

--- shingou_action.py
+++ shingou_action.py
@@ -1,22 +1,28 @@
-# shingou_action.py
-from enum import Enum
 
+from enum import Enum
+import sys
 # 信号の列挙型
 class Shingou(Enum):
-    TOMARE = 1    # とまれ
-    SUSUME = 2    # すすめ
-    CHUI = 3      # ちゅうい
+    RED = 1    # とまれ
+    BLUE = 2    # すすめ
+    YELLOW = 3      # ちゅうい
 
 # 信号に対応する処理を行う関数
 def act_shingou(color: int) -> Shingou:
     if color == 1:
         print("とまれ")
-        return Shingou.TOMARE
+        return Shingou.RED
     elif color == 2:
         print("すすめ")
-        return Shingou.SUSUME
+        return Shingou.BLUE
     elif color == 3:
         print("ちゅうい")
-        return Shingou.CHUI
+        return Shingou.YELLOW
     else:
         raise ValueError("信号機の色に対応していません")
+
+try:
+    color_input = int(sys.argv[1])
+    act_shingou(color_input)
+except ValueError as e:
+    print(f"エラー：{e}")
